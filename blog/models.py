f1078eb931259c84959e8d3a0f1d7aac446af7ea
williantag07 <williantag07@gmail.com>
2021-11-04 07:29:29 -0700

    My Django Girls app, first commit

--- blog/models.py
+++ blog/models.py
@@ -10,11 +10,68 @@ class Post (models.Model):
     titre = models.CharField(max_length=200)
     texte = models.TextField()  # ICI blank= True , null= True 'BAS : Les types CHAR et TEXT ne sont jamais enregistrés comme NULL par Django, donc null=True n'est pas nécessaire. Cependant, vous pouvez définir manuellement l'un de ces champs sur Aucun pour le forcer à être défini sur NULL. Si vous avez un scénario où cela pourrait être nécessaire, vous devez toujours inclure null=True.'
     date_de_creation = models.DateTimeField (default= timezone.now())
-    date_de_publication =models.DateTimeField(blank= True , null= True) # (blank=True) lève IntegrityError si vide,) # NULL autorisé, mais doit être rempli dans un formulaire
+    date_de_publication =models.DateTimeField(blank= True , null= True)   #methode : publication               # (blank=True) lève IntegrityError si vide,) # NULL autorisé, mais doit être rempli dans un formulaire
 
-    def publicatoion (self):
-        self.date_de_publication = timezone.now() # cest ma methode publication cf:ma modelisation de l'objet Post
-        self.save()
+    def publication (self): # cest ma methode (ou action) publication  cf:ma modelisation de l'objet Post.
+         self.date_de_publication = timezone.now() #  cette ligne veut dire que de 'date_de_publication' j'attend quoi? ou je veux quoi? ici par exemple , de la date de publication on veut l'heure actuel commender par 'timezone.now()'
+         self.save()
 
     def __str__(self):
         return self.titre # ON on btiendra le text et le titre du post
+
+
+class Chat(models.Model):
+    color = models.CharField (blank=True, null=True)
+    date_of_birth = models.DateTimeField(blank=True , null= True)
+    proprietaire = models.CharField(nom ='nom proprietaire', blank=True, null=True) # methode: personne
+    nourriture = models.CharField(blank=True, null=True) # methode: nourriture_pour_chat
+
+
+    def personne(self):
+        self.proprietaire = timezone.now() # juste pour le moment on veut la timezone et apres on va mieux modeliser
+        self.save()
+
+    def nourriture_pour_chat(self):
+            self.nourriture = timezone.now()
+            self.save()
+
+    def __str__(self):
+        return self.proprietaire
+
+
+class Chat1(models.Model):
+    color = models.CharField(blank=True, null=True)
+    date_of_birth = models.DateTimeField(blank=True, null=True)
+    proprietaire1 = models.CharField(nom='nom proprietaire1', blank=True, null=True)  # methode: personne
+    nourriture = models.CharField(blank=True, null=True)  # methode: nourriture_pour_chat
+
+    def personne1(self):
+        self.proprietaire1 = timezone.now()  # juste pour le moment on veut la timezone et apres on va mieux modeliser
+        self.save()
+
+    def nourriture_pour_chat1(self):
+        self.nourriture=timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.proprietaire1
+
+
+
+class Chat2(models.Model):
+    color = models.CharField(blank=True, null=True)
+    date_of_birth = models.DateTimeField(blank=True, null=True)
+    proprietaire2 = models.CharField(nom='nom proprietaire2', blank=True, null=True)  # methode: personne
+    nourriture = models.CharField(blank=True, null=True)  # methode: nourriture_pour_chat
+
+    def personne2(self):
+        self.proprietaire2 = timezone.now()  # juste pour le moment on veut la timezone et apres on va mieux modeliser
+        self.save()
+
+    def nourriture_pour_chat2(self):
+        self.nourriture=timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.proprietaire2
+
